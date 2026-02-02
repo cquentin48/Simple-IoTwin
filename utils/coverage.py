c09a084c880192ea0ae05670e391da00af5b6fe5
Quentin CHAPEL <chapelquen@gmail.com>
2026-02-02 10:24:10 +0100

    [Test]Python coverage script percentage output

--- utils/coverage.py
+++ utils/coverage.py
@@ -3,9 +3,11 @@ import subprocess
 sh_coverage_cmd = "lcov --summary cov_result.info | grep 'lines.......:' | awk '{print substr($2,1, length($2)-1)}'"
 ps = subprocess.Popen(sh_coverage_cmd, shell=True, stdout=subprocess.PIPE, stderr = subprocess.STDOUT)
 output = ps.communicate()[0]
+
+print(f"Current coverage percentage : {output}")
+
 coverage_percentage = float(str(output).replace("b'","").replace("\\n'",""))
 
-print(f"Current coverage percentage : {coverage_percentage}%")
 
 if coverage_percentage > 80.0:
     print(f"The coverage percentage must be at least at {80.0}%!")
