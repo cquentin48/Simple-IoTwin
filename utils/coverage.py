2accb0b0d50f2c9b16a16d14cea285295709d2d1
Quentin CHAPEL <chapelquen@gmail.com>
2026-02-02 10:37:53 +0100

    [Fix]Python code coverage script

--- utils/coverage.py
+++ utils/coverage.py
@@ -4,13 +4,21 @@ sh_coverage_cmd = "lcov --summary cov_result.info"
 ps = subprocess.Popen(sh_coverage_cmd, shell=True, stdout=subprocess.PIPE, stderr = subprocess.STDOUT)
 output = ps.communicate()[0]
 
-if output == b'':
-    print("No percentage found!")
-    exit(1)
+if '\n' in lines:
+    output = [detail.strip()
+              for detail in output.split('\n')
+              if detail.strip().startswith('lines')
+              ][0]
+    coverage_percentage = output.split(': ')[1].split(' ')[0]
+    coverage_percentage = float(coverage_percentage.replace('%',''))
+else:
+    if output == b'':
+        print("No percentage found!")
+        exit(1)
 
-print(f"Current coverage percentage : {output}")
+    print(f"Current coverage percentage : {output}")
 
-coverage_percentage = float(str(output).replace("b'","").replace("\\n'",""))
+    coverage_percentage = float(str(output).replace("b'","").replace("\\n'",""))
 
 
 if coverage_percentage > 80.0:
